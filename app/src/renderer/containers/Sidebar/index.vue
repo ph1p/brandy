763ca8920a3712ac18a6887b0ed41756ccaa3b32
ph1p <phil@capshake.com>
2018-08-01 13:12:42 +0200

    refactor(presets): export presets

--- app/src/renderer/containers/Sidebar/index.vue
+++ app/src/renderer/containers/Sidebar/index.vue
@@ -132,6 +132,8 @@
 import { remote, ipcRenderer } from 'electron';
 import Filters from '../../components/Filters';
 
+import { presets, filterPresets } from '../../presets';
+
 export default {
   name: 'sidebar',
   components: {
@@ -161,7 +163,9 @@ export default {
       filter: {
         blur: 0,
         filters: []
-      }
+			},
+			filterPresets,
+			presets
     };
   },
   watch: {
@@ -199,128 +203,6 @@ export default {
     }
   },
   computed: {
-    presets() {
-      return [
-        {
-          icon: 'instagram',
-          title: 'Instagram',
-          width: 1080,
-          height: 1080,
-          logo: 180,
-          logo2: 95,
-          fontSize: 120
-        },
-        {
-          icon: 'instagram',
-          title: 'Instagram big',
-          width: 1080,
-          height: 1350,
-          logo: 180,
-          logo2: 95,
-          fontSize: 120
-        },
-        {
-          icon: 'twitter',
-          title: 'Twitter header',
-          width: 1500,
-          height: 500,
-          logo: 220,
-          logo2: 95,
-          fontSize: 80
-        },
-        {
-          icon: 'twitter',
-          title: 'Twitter desktop grid',
-          width: 1200,
-          height: 1200,
-          logo: 160,
-          logo2: 95,
-          fontSize: 130
-        },
-        {
-          icon: 'twitter',
-          title: 'Twitter mobile grid',
-          width: 1200,
-          height: 675,
-          logo: 160,
-          logo2: 95,
-          fontSize: 110
-        },
-        {
-          icon: 'twitter',
-          title: 'Twitter appcard mit Bild',
-          width: 800,
-          height: 320,
-          logo: 120,
-          logo2: 95,
-          fontSize: 60
-        },
-        {
-          icon: 'facebook',
-          title: 'Facebook ad',
-          width: 1200,
-          height: 628,
-          logo: 160,
-          logo2: 95,
-          fontSize: 110
-        },
-        {
-          icon: 'facebook',
-          title: 'Facebook carousel',
-          width: 1080,
-          height: 1080,
-          logo: 180,
-          logo2: 95,
-          fontSize: 120
-        }
-      ];
-    },
-    filterPresets() {
-      return [
-        {
-          name: 'spotify',
-          blur: 0,
-          filters: [
-            {
-              color: '#08b76c',
-              opacity: 1,
-              mode: 'multiply'
-            },
-            {
-              color: '#0a3463',
-              opacity: 1,
-              mode: 'lighten'
-            }
-          ]
-        },
-        {
-          name: 'law&order',
-          blur: 0,
-          filters: [
-            // {
-            //   opacity: 1,
-            //   mode: 'unset'
-            // },
-            {
-              color: '#c35655',
-              opacity: 1,
-              mode: 'screen'
-            }
-          ]
-        },
-        {
-          name: 'dark',
-          blur: 0.1,
-          filters: [
-            {
-              color: '#EF3340',
-              opacity: 1,
-              mode: 'darken'
-            }
-          ]
-        }
-      ];
-    },
     logoProperties() {
       return {
         brand: this.brand,
---
+++ app/src/renderer/presets/index.js
@@ -0,0 +1,119 @@
+export const presets = [
+  {
+    icon: 'instagram',
+    title: 'Instagram',
+    width: 1080,
+    height: 1080,
+    logo: 180,
+    logo2: 95,
+    fontSize: 120
+  },
+  {
+    icon: 'instagram',
+    title: 'Instagram big',
+    width: 1080,
+    height: 1350,
+    logo: 180,
+    logo2: 95,
+    fontSize: 120
+  },
+  {
+    icon: 'twitter',
+    title: 'Twitter header',
+    width: 1500,
+    height: 500,
+    logo: 220,
+    logo2: 95,
+    fontSize: 80
+  },
+  {
+    icon: 'twitter',
+    title: 'Twitter desktop grid',
+    width: 1200,
+    height: 1200,
+    logo: 160,
+    logo2: 95,
+    fontSize: 130
+  },
+  {
+    icon: 'twitter',
+    title: 'Twitter mobile grid',
+    width: 1200,
+    height: 675,
+    logo: 160,
+    logo2: 95,
+    fontSize: 110
+  },
+  {
+    icon: 'twitter',
+    title: 'Twitter appcard mit Bild',
+    width: 800,
+    height: 320,
+    logo: 120,
+    logo2: 95,
+    fontSize: 60
+  },
+  {
+    icon: 'facebook',
+    title: 'Facebook ad',
+    width: 1200,
+    height: 628,
+    logo: 160,
+    logo2: 95,
+    fontSize: 110
+  },
+  {
+    icon: 'facebook',
+    title: 'Facebook carousel',
+    width: 1080,
+    height: 1080,
+    logo: 180,
+    logo2: 95,
+    fontSize: 120
+  }
+];
+
+export const filterPresets = [
+  {
+    name: 'spotify',
+    blur: 0,
+    filters: [
+      {
+        color: '#08b76c',
+        opacity: 1,
+        mode: 'multiply'
+      },
+      {
+        color: '#0a3463',
+        opacity: 1,
+        mode: 'lighten'
+      }
+    ]
+  },
+  {
+    name: 'law&order',
+    blur: 0,
+    filters: [
+      // {
+      //   opacity: 1,
+      //   mode: 'unset'
+      // },
+      {
+        color: '#c35655',
+        opacity: 1,
+        mode: 'screen'
+      }
+    ]
+  },
+  {
+    name: 'dark',
+    blur: 0.1,
+    filters: [
+      {
+        color: '#EF3340',
+        opacity: 1,
+        mode: 'darken'
+      }
+    ]
+  }
+];
